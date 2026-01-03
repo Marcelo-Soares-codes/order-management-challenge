7bb417f305df67f1d59fed70752cbebd9d0bcb76
Marcelo Soares <marcelo.soares1070@gmail.com>
2026-01-03 12:35:11 -0300

    refactor: organize database conection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import { app } from "./app";
+import { connectDatabase } from "./shared/database";
 
 const PORT = process.env.PORT || 3333;
 
 async function bootstrap() {
-  await mongoose.connect(process.env.MONGO_URI!);
-  console.log("MongoDB connected");
+  await connectDatabase(process.env.MONGO_URI!);
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
